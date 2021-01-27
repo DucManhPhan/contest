28c6d3361081757a73cbfa3be203d629c1a109f0
Phan Manh <ducmanhphan93@gmail.com>
2021-01-27 23:07:52 +0700

    update files

--- LeetCode/two-pointers/src/com/manhpd/LongPressedName.java
+++ LeetCode/two-pointers/src/com/manhpd/LongPressedName.java
@@ -6,8 +6,17 @@ package com.manhpd;
 public class LongPressedName {
 
     public static void main(String[] args) {
+//        String name = "alex";
+//        String typed = "aaleex";
+
         String name = "alex";
-        String typed = "aaleex";
+        String typed = "aaleexa";
+
+//        String name = "saeed";
+//        String typed = "ssaaedd";
+
+//        String name = "leelee";
+//        String typed = "lleeelee";
 
 //        String name = "saeed";
 //        String typed = "ssaaedd";
@@ -26,25 +35,35 @@ public class LongPressedName {
     public boolean isLongPressedName(String name, String typed) {
         int pName = 0;
         int pTyped = 0;
-
         char[] names = name.toCharArray();
         char[] typeds = typed.toCharArray();
 
-        while (pName < names.length || pTyped < typeds.length) {
+        while (pName < names.length && pTyped < typeds.length) {
+            int nDuplicatedName = 1;
+            while (pName + 1 < names.length && names[pName] == names[pName + 1]) {
+                pName = pName + 1;
+                ++nDuplicatedName;
+            }
+
             char cName = names[pName];
             char cTyped = typeds[pTyped];
-
             if (cName != cTyped) {
                 return false;
             }
 
+            int nDuplicatedTyped = 0;
             while (cName == cTyped) {
                 ++pTyped;
+                ++nDuplicatedTyped;
                 if (pTyped < typeds.length) {
                     cTyped = typeds[pTyped];
                 } else {
                     break;
                 }
+            }
+
+            if (nDuplicatedName > nDuplicatedTyped) {
+                return false;
             }
 
             ++pName;
