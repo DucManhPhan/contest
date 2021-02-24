57c940c75aa46a6dbd57f6785e4b5d307f97514a
Phan Manh <ducmanhphan93@gmail.com>
2021-02-25 00:33:54 +0700

    update files

---
+++ Hackerrank/problem-solving/src/com/manhpd/SuperReducedString.java
@@ -0,0 +1,22 @@
+package com.manhpd;
+
+/**
+ * https://www.hackerrank.com/challenges/reduced-string/problem
+ */
+public class SuperReducedString {
+
+    public static void main(String[] args) {
+        String s = "aaabccddd";
+        System.out.println(superReducedString(s));
+    }
+
+    public static String superReducedString(String s) {
+
+        return "";
+    }
+
+    public static void superReducedString(char[] chars, int start) {
+
+    }
+
+}
