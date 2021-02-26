84da4c3b5bdaaf5034c1a6aa130fe2a54876b637
Phan Manh <ducmanhphan93@gmail.com>
2021-02-26 08:08:37 +0700

    update files

--- Hackerrank/problem-solving/src/com/manhpd/SuperReducedString.java
+++ Hackerrank/problem-solving/src/com/manhpd/SuperReducedString.java
@@ -6,17 +6,38 @@ package com.manhpd;
 public class SuperReducedString {
 
     public static void main(String[] args) {
-        String s = "aaabccddd";
+//        String s = "aaabccddd";
+        String s = "abba";
         System.out.println(superReducedString(s));
     }
 
     public static String superReducedString(String s) {
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = 1;
+        int excludedChars = 0;
 
-        return "";
-    }
+        while (end < chars.length - excludedChars) {
+            if (chars[start] == chars[end] && end <= chars.length - 1) {
+                int i = start;
+                int j = end;
+                excludedChars += 2;
+                while (i < chars.length - excludedChars) {
+                    chars[i] = chars[j + 1];
+                    ++i;
+                    ++j;
+                }
+            } else {
+                ++start;
+                ++end;
+            }
+        }
 
-    public static void superReducedString(char[] chars, int start) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length - excludedChars; ++i) {
+            sb.append(chars[i]);
+        }
+        return sb.toString();
     }
 
 }
