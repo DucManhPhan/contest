c87f0be640f6d40b734397e8c33e14ca09ad10dd
Phan Manh <ducmanhphan93@gmail.com>
2021-03-01 23:27:52 +0700

    update files

--- Hackerrank/problem-solving/src/com/manhpd/SuperReducedString.java
+++ Hackerrank/problem-solving/src/com/manhpd/SuperReducedString.java
@@ -1,5 +1,7 @@
 package com.manhpd;
 
+import java.util.Stack;
+
 /**
  * https://www.hackerrank.com/challenges/reduced-string/problem
  */
@@ -7,36 +9,62 @@ public class SuperReducedString {
 
     public static void main(String[] args) {
 //        String s = "aaabccddd";
-        String s = "abba";
-        System.out.println(superReducedString(s));
+//        String s = "abba";
+        String s = "zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh";
+        String result = "tqauhujtmxnsbzpykwlvpfyqijvdhuhirdmuxiobyvxupqwydkpbxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh";
+        System.out.println(superReducedString1(s));
+
+        System.out.println(superReducedString1(s).equals(result));
     }
 
     public static String superReducedString(String s) {
-        char[] chars = s.toCharArray();
-        int start = 0;
-        int end = 1;
-        int excludedChars = 0;
+        StringBuilder sb = new StringBuilder(s);
 
-        while (end < chars.length - excludedChars) {
-            if (chars[start] == chars[end] && end <= chars.length - 1) {
-                int i = start;
-                int j = end;
-                excludedChars += 2;
-                while (i < chars.length - excludedChars) {
-                    chars[i] = chars[j + 1];
-                    ++i;
-                    ++j;
-                }
-            } else {
-                ++start;
-                ++end;
+        for (int i = 1; i < sb.length(); i++) {
+            if (sb.charAt(i) == sb.charAt(i - 1)) {
+                sb.delete(i - 1, i + 1);
+                i = 0;
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < chars.length - excludedChars; ++i) {
-            sb.append(chars[i]);
+        if (sb.length() == 0) {
+            return "Empty String";
         }
+
+        return sb.toString();
+    }
+
+    /**
+     * Using stack data structure
+     *
+     * @param s
+     * @return
+     */
+    public static String superReducedString1(String s) {
+        Stack<Character> stk = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (!stk.isEmpty()) {
+                char topChar = stk.peek();
+                if (topChar == c) {
+                    stk.pop();
+                } else {
+                    stk.push(c);
+                }
+            } else {
+                stk.push(c);
+            }
+        }
+
+        if (stk.size() == 0) {
+            return "Empty String";
+        }
+
+        stk.forEach(c -> {
+            sb.append(c);
+        });
+
         return sb.toString();
     }
 
