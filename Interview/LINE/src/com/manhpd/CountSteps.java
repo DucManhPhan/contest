58098e1038dd75b4f7f9eff0abbe794ced9430a9
Phan Manh <ducmanhphan93@gmail.com>
2022-03-13 16:16:24 +0700

    update files

---
+++ Interview/LINE/src/com/manhpd/CountSteps.java
@@ -0,0 +1,30 @@
+package com.manhpd;
+
+/**
+ * Given an integer under the binary format of a string: "011100"
+ *
+ * We will do two operations:
+ * - if an integer is even, divide to 2.
+ * - if an integer is odd, substract to 1.
+ *
+ * Calculate how many steps of these operations?
+ *
+ * The length of string can be [1, 1000,000]
+ *
+ * Example 1:
+ * Input: s = "011100"
+ * Output: 7
+ *
+ */
+public class LineInterviewApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int countNumberToZero(String s) {
+
+        return 0;
+    }
+
+}
