bf016c025db517b3080a1b1c10fe38fe72ed3a6e
Phan Manh <ducmanhphan93@gmail.com>
2022-03-13 17:23:07 +0700

    update files

--- Interview/LINE/src/com/manhpd/CountSteps.java
+++ Interview/LINE/src/com/manhpd/CountSteps.java
@@ -1,13 +1,15 @@
 package com.manhpd;
 
 /**
- * Given an integer under the binary format of a string: "011100"
+ * Ref: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+ *
+ * Given an integer under the binary format written as a string: "011100"
  *
  * We will do two operations:
  * - if an integer is even, divide to 2.
- * - if an integer is odd, substract to 1.
+ * - if an integer is odd, subtract to 1.
  *
- * Calculate how many steps of these operations?
+ * Then, we will do these operations until a result is 0. Calculate how many steps of these operations?
  *
  * The length of string can be [1, 1000,000]
  *
