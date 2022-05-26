f0402337d83dd74699c81e45e4d1326be14affe4
manhpd <ducmanhphan93@gmail.com>
2022-05-26 10:47:56 +0700

    update files

--- LeetCode/binary-search/src/com/manhpd/FindMinOfRotatedArray.java
+++ LeetCode/binary-search/src/com/manhpd/FindMinOfRotatedArray.java
@@ -20,13 +20,20 @@ public class FindMinOfRotatedArray {
 
     public static void main(String[] args) {
 //        int[] nums = {3,4,5,1,2};
-//        int[] nums = {2, 1};
-        int[] nums = {1, 2};
+        int[] nums = {2, 1};
+//        int[] nums = {1, 2};
 
-        int res = findMin(nums);
+//        int res = findMin(nums);
+        int res = findMinV2(nums);
         System.out.println(res);
     }
 
+    /**
+     * Using template 2 of binary search
+     *
+     * @param nums
+     * @return
+     */
     public static int findMin(int[] nums) {
         if (nums == null) {
             return -1;
@@ -46,6 +53,29 @@ public class FindMinOfRotatedArray {
         }
 
         return nums[left];
+    }
+
+    /**
+     * Using template 3
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMinV2(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[low] > nums[high] ? nums[high] : nums[low];
     }
 
 }
