488d6ac66b5adf17d8d27dea73d6e5ede13ec36c
Phan Manh <ducmanhphan93@gmail.com>
2022-05-27 07:29:16 +0700

    update files

--- LeetCode/linked-list/src/com/manhpd/EvenOddLinkedList.java
+++ LeetCode/linked-list/src/com/manhpd/EvenOddLinkedList.java
@@ -90,20 +90,3 @@ public class EvenOddLinkedList {
     }
 
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
