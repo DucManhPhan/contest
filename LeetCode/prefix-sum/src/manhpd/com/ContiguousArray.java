c5e8bc0d345f45b6b9ab83a3265ebad2dda67ce6
manhpd <ducmanhphan93@gmail.com>
2026-06-10 23:55:31 +0700

    update files

---
+++ LeetCode/prefix-sum/src/manhpd/com/ContiguousArray.java
@@ -0,0 +1,25 @@
+package manhpd.com;
+
+/**
+ * Refer: https://leetcode.com/problems/contiguous-array/description/
+ */
+public class ContiguousArray {
+    public static void main(String[] args) {
+        int[] nums = {0, 1};
+        int expected = 2;
+
+//        int[] nums = {0, 1, 0};
+//        int expected = 2;
+
+//        int[] nums = {0, 1, 1, 1, 1, 1, 0, 0, 0};
+//        int expected = 6;
+
+        int res = findMaxLength(nums);
+        System.out.println("Resutlt: " + res);
+    }
+
+    private static int findMaxLength(int[] nums) {
+
+        return 0;
+    }
+}
